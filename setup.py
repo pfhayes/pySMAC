4848bb54e6e47de9efee5a4314a6b25c2007b4e7
sfalkner <damoppi@yahoo.de>
2015-03-31 23:22:30 +0200

    removed mpldatacursor dependency in setup.py.

--- setup.py
+++ setup.py
@@ -47,12 +47,11 @@ setup(
     version = "0.9",
     packages = find_packages(),
     install_requires = ['docutils>=0.3', 'setuptools', 'numpy', 'matplotlib'],
-    extras_require = {'interactive_plots': 'git+https://github.com/joferkington/mpldatacursor.git'},
+    #extras_require = {'interactive_plots': 'git+https://github.com/joferkington/mpldatacursor.git'},
     author = "Stefan Falkner and Tobias Domhan (python wrapper). Frank Hutter, Holger Hoos, Kevin Leyton-Brown, Kevin Murphy and Steve Ramage (SMAC)",
     author_email = "sfalkner@informatik.uni-freiburg.de",
     description = "python interface to the hyperparameter optimization tool SMAC.",
     include_package_data = True,
-    #test_suite = 'pysmac.test',
     keywords = "hyperparameter parameter optimization hyperopt bayesian smac global",
     license = "SMAC is free for academic & non-commercial usage. Please contact Frank Hutter(fh@informatik.uni-freiburg.de) to discuss obtaining a license for commercial purposes.",
     url = "http://www.cs.ubc.ca/labs/beta/Projects/SMAC/"
